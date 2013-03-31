2e7b52dd63df3090441903cc4f41bd607bc1d2ac
saurabhnanda <saurabhnanda@gmail.com>
2013-03-31 13:57:01 +0530

    * rename file * Cleaned up Trip code

--- models/trip.rb
+++ models/trip.rb
@@ -1,6 +1,7 @@
 require 'rubygems'
 require 'pg'
 require 'time'
+
 class Trip
 	attr_accessor :id,:name,:email,:airport,:arrival_datetime,:flight_no,:time_tolerance,:km_tolerance,:address,:lat,:long,:phone,:status
 
@@ -21,61 +22,39 @@ class Trip
 
 	def save
 		conn = PG::Connection.open(:host => "localhost",:dbname => 'sharecab_db', :user => "sharecab", :password => "sharecab")
-		
-		@arrival_datetime = arrival_datetime.utc
-		@arrival_datetime = @arrival_datetime.strftime("%F %T")
-		res = conn.exec("Insert into Trip (name,email,airport,arrival_datetime,flight_no,time_tolerance,km_tolerance,address,lat,long,phonenumber,status) values('#{@name}','#{@email}','#{@airport}','#{@arrival_datetime}','#{@flight_no}',#{@time_tolerance},#{@km_tolerance},'#{@address}',#{@lat},#{@long},'#{@phone}','#{@status}');")
-		puts res
+		adt = @arrival_datetime.getutc.strftime('%F %T')
+		# @arrival_datetime = arrival_datetime.utc
+		# TODO -- Prevent SQL Injection
+		res = conn.exec("Insert into Trip (name,email,airport,arrival_datetime,flight_no,time_tolerance,km_tolerance,address,lat,long,phonenumber,status) values('#{@name}','#{@email}','#{@airport}','#{adt}','#{@flight_no}',#{@time_tolerance},#{@km_tolerance},'#{@address}',#{@lat},#{@long},'#{@phone}','#{@status}');")
 	end
 
 	def self.fetch(id)
 		conn = PG::Connection.open(:host => "localhost",:dbname => 'sharecab_db', :user => "sharecab", :password => "sharecab")
 		sql = "Select name,email,airport,arrival_datetime,flight_no,time_tolerance,km_tolerance,address,lat,long,phonenumber,status from Trip where id = #{id}"
 		res = conn.exec(sql)
-		a = res.values
-		b = a[0]
-
-		time = Time.new
-		timestring = b[3] + "UTC"
-		puts timestring
-		time = Time.parse(timestring)
-		time = time.getlocal
-		t1 = Trip.new(b[0],b[1],b[2],time,b[4],b[5],b[6],b[7],b[8],b[9],b[10],b[11])
-		return t1
+		b = res.values[0]
+		time = Time.parse(b[3] + "UTC").getlocal("+0530")
+		return Trip.new(b[0],b[1],b[2],time,b[4],b[5],b[6],b[7],b[8],b[9],b[10],b[11])
 	end
 
-	def match_trips()
+	def match_trips
 		conn = PG::Connection.open(:host => "localhost",:dbname => 'sharecab_db', :user => "sharecab", :password => "sharecab")
 		usertime = Time.new
-		@arrival_datetime = arrival_datetime.utc
-		usertime = @arrival_datetime.strftime("%F %T")  
+		usertime = @arrival_datetime.getutc.strftime("%F %T")  
 		sql = "Select name,email,airport,arrival_datetime,flight_no,time_tolerance,km_tolerance,address,lat,long,phonenumber,status from Trip where airport = '#{@airport}' and abs(extract(epoch from (timestamp '#{usertime}' - arrival_datetime))) < (#{@time_tolerance}*60)"
 		res = conn.exec(sql)
 		tripobjects = []
 		a = res.values
 		time = Time.new
 
-		(0...a.length).each do |k|
-			b = a[k]
-			timestring = b[3] + "UTC"
-			time = Time.parse(timestring)
-			time = time.getlocal
-			t1 = Trip.new(b[0],b[1],b[2],time,b[4],b[5],b[6],b[7],b[8],b[9],b[10],b[11])
-			tripobjects.push(t1)
+		res.values.each do |b|
+			time = Time.parse(b[3] + "UTC").getlocal("+0530")
+			tripobjects.push(Trip.new(b[0],b[1],b[2],time,b[4],b[5],b[6],b[7],b[8],b[9],b[10],b[11]))
 		end
+
 		return tripobjects
 	end
 
 	def validate
 	end
-end
-# time = Time.now
-# t = Trip.new("a","b","c",time,"e",10,20,"i",104.2222222,178.1111111,"9158002020")
-# t.save
-t1 = Trip.fetch(2)
-b = []
-b = t1.match_trips
-b.each do |k|
-	puts k.inspect
-	puts "\n---------------------------------------------------------------------\n"
 end
